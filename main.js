f16d1d2f129d2de426b6a6d3193c1ec70a7a5883
josnethmoreno <josnethmoreno@gmail.com>
2023-03-05 18:49:50 -0400

    time

--- main.js
+++ main.js
@@ -6,7 +6,9 @@ const toggleTheme = document.getElementById('toggleTheme')
 let theme = localStorage.theme || ''
 
 if (!localStorage.theme) {
-	theme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
+	theme = window.matchMedia('(prefers-color-scheme: light)').matches
+		? 'light'
+		: 'dark'
 }
 
 document.documentElement.classList.add(theme)
@@ -30,3 +32,49 @@ const changeTheme = () => {
 }
 
 toggleTheme.addEventListener('click', changeTheme)
+
+function isTouchDevice() {
+	return (
+		'ontouchstart' in window ||
+		navigator.maxTouchPoints > 0 ||
+		navigator.msMaxTouchPoints > 0
+	)
+}
+
+const isTouch = isTouchDevice()
+
+if (!isTouch) {
+	const cursor = document.getElementById('cursor')
+
+	const onMouseMove = (e) => {
+		cursor.style.setProperty('--x', e.clientX + 'px')
+		cursor.style.setProperty('--y', e.clientY + 'px')
+	}
+
+	const cursorEvents = () => {
+		document.addEventListener('mousemove', onMouseMove)
+		handleHoverEvents()
+	}
+
+	const handleHoverEvents = () => {
+		document.querySelectorAll('[data-cursor="hover"]').forEach((el) => {
+			el.addEventListener('mouseover', () =>
+				cursor.setAttribute('data-action', 'true')
+			)
+			el.addEventListener('mouseout', () =>
+				cursor.setAttribute('data-action', 'false')
+			)
+		})
+	}
+	cursorEvents()
+}
+
+const hour = document.getElementById('hour')
+
+const date = new Date()
+hour.innerText = `${date.getHours()}:${date.getMinutes()}`
+
+setInterval(() => {
+	const date = new Date()
+	hour.innerText = `${date.getHours()}:${date.getMinutes()}`
+}, 1000)
