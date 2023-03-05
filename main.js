c1975c8d262a2e5711efb03588623874adf01aa4
josnethmoreno <josnethmoreno@gmail.com>
2023-03-05 15:12:42 -0400

    Links

--- main.js
+++ main.js
@@ -1,2 +1,32 @@
 import './style.css'
 import './reset.css'
+
+const toggleTheme = document.getElementById('toggleTheme')
+
+let theme = localStorage.theme || ''
+
+if (!localStorage.theme) {
+	theme = window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark'
+}
+
+document.documentElement.classList.add(theme)
+
+const changeTheme = () => {
+	if (theme === 'light') {
+		theme = 'dark'
+		document.documentElement.classList.remove('light')
+		document.documentElement.classList.add(theme)
+		localStorage.theme = theme
+		return
+	}
+
+	if (theme === 'dark') {
+		theme = 'light'
+		document.documentElement.classList.remove('dark')
+		document.documentElement.classList.add(theme)
+		localStorage.theme = theme
+		return
+	}
+}
+
+toggleTheme.addEventListener('click', changeTheme)
